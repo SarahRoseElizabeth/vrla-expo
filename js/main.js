add8853efcec06d75ff87ddddd0bbdb7d9e6912f
Sarah Bartley <roseelizabeth.sarah@gmail.com>
2018-07-07 15:53:29 -0700

    Changes to collapsible list

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+$(document).ready(function(){
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        console.log(content);
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    })};
+    let collapsible = $(".collapsible");
+
+    collapsible.click(function() {
+        let $self = $(this);
+        $self.toggleClass("active");
+
+    });
+});
+
+
+
+
+
 
