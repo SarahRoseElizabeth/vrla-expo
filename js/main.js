69166ad6a3bef1d8f3f4690dba0ab3dd6098e86e
Sarah Bartley <roseelizabeth.sarah@gmail.com>
2018-07-06 18:17:34 -0700

    Table feature finished, new features in progress

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = document.getElementsByClassName("event-list");
+        if (content.style.display === "none") {
+            content.style.display = "block";
+        } else {
+            content.style.display = "block";
+        }
+    })};
