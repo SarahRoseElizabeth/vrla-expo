2e42058888d7b9ed8c3349b0710fffec4238078e
Sarah Bartley <roseelizabeth.sarah@gmail.com>
2018-07-07 21:22:12 -0700

    Nav menu, positioning

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
 
-    let collapsible = $(".collapsible");
+    let collapsible = $(".collapsible"),
+        menu1 = $(".desktop-ham-icon");
 
     collapsible.click(function() {
         let $self = $(this);
         $self.toggleClass("active");
 
+    });
+
+    menu1.click(function() {
+        let menuWrapper1 = $(".desktop-menu-wrapper");
+        menuWrapper1.toggleClass("menu-click");
     });
 });
 
