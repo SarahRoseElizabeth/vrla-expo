63b401c676bbdccc284dd871bc4f4bca973fd24f
Sarah Bartley <roseelizabeth.sarah@gmail.com>
2018-07-12 12:45:36 -0700

    Refactor + readme

--- js/main.js
+++ js/main.js
@@ -8,15 +8,12 @@ $(document).ready(function(){
     collapsible.click(function() {
         let $self = $(this);
         $self.toggleClass("active");
-
     });
 
     menu1.click(function() {
         let menuWrapper1 = $(".desktop-menu-wrapper");
         menuWrapper1.toggleClass("menu-click");
     });
-
-
 
     let tlCloud = new TimelineMax({repeat: -1});
     let tlCloud2 = new TimelineMax({repeat: -1});
@@ -27,13 +24,13 @@ $(document).ready(function(){
         .to("#cloud1",3,{y:'-=5', ease: Sine.easeInOut})
         .to("#cloud1",3,{y:'+=5', ease: Sine.easeInOut});
 
-    tlCloud2.
-    to("#cloud2",2,{y:'+=7', ease: Sine.easeInOut}).
-    to("#cloud2",2,{y:'-=7', ease: Sine.easeInOut});
+    tlCloud2
+        .to("#cloud2",2,{y:'+=7', ease: Sine.easeInOut})
+        .to("#cloud2",2,{y:'-=7', ease: Sine.easeInOut});
 
-    tlCloud3.
-    to("#cloud3",3,{y:'-=7', ease: Sine.easeInOut}).
-    to("#cloud3",3,{y:'+=7', ease: Sine.easeInOut});
+    tlCloud3
+        .to("#cloud3",3,{y:'-=7', ease: Sine.easeInOut})
+        .to("#cloud3",3,{y:'+=7', ease: Sine.easeInOut});
 
     masterCloud.add(tlCloud2);
     masterCloud.add(tlCloud, 3);
@@ -45,7 +42,6 @@ $(document).ready(function(){
     tlLeaves
         .to("#leaf",16,{x:'+=200', ease: Power0.linear, repeat: -1}, 0)
         .to("#leaf",5,{y:'+=10', ease: Power1.easeInOut, yoyo: true, repeat: -1}, 3);
-
 
     tlLeaves
         .to("#leaf-2",14,{x:'+=200', ease: Power0.linear, repeat: -1}, 0)
@@ -82,7 +78,6 @@ $(document).ready(function(){
     tlLeaves
         .to("#leaf-9",17,{x:'+=200', ease: Power0.linear, repeat: -1}, 0)
         .to("#leaf-9",5,{y:'+=10', ease: Power1.easeInOut, yoyo: true, repeat: -1}, 7);
-
 });
 
 
